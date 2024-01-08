fe3538a3f54dac8fe91bf9f17cb2f24773715f52
minh <luongvanminh12022002@gmail.com>
2024-01-08 17:56:47 +0700

    model configure

---
+++ src/main/java/com/example/microservice_demo/Model/Order.java
@@ -0,0 +1,31 @@
+package com.example.microservice_demo.Model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.aspectj.weaver.ast.Or;
+import org.hibernate.annotations.Columns;
+
+import java.util.List;
+
+@Table(name = "\"order\"")
+@Entity
+@Data
+public class Order {
+
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "orders")
+    private User user;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "order")
+    private List<Product> products;
+}
