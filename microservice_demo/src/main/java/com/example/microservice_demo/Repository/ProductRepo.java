f439aa12e163fec913fce1fcf0b6e28c8e27199b
minh <luongvanminh12022002@gmail.com>
2024-01-11 15:03:57 +0700

    create authentication service

--- microservice_demo/src/main/java/com/example/microservice_demo/Repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservice_demo.Repository;
-
-import com.example.microservice_demo.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product, Long> {
-}
