7b42bc659fa1244b32ecb234e40cfdcd191484df
Luong Van Minh <minhlv.atomi@lpb.lv.net>
2024-01-18 15:05:25 +0700

    rename

--- order_service/src/main/java/com/example/microservice_demo/Controller/getInforUser.java
+++ order_service/src/main/java/com/example/microservice_demo/Controller/getInforUser.java
@@ -1,7 +1,6 @@
 package com.example.microservice_demo.Controller;
 
 import com.example.microservice_demo.Controller.Reponse.UserResponse;
-import lombok.Data;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "customer-service")
 @Component
 
-public interface GetInforUser {
+public interface getInforUser {
 
     @GetMapping("/customer/{id}")
     public ResponseEntity<UserResponse> getInfor(@PathVariable Long id);
