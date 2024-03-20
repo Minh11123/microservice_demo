41185cd3b7f5db1b1950610c4f6158f825acafd9
Minh11123 <luongvanminh12022002@gmail.com>
2024-03-20 21:00:03 +0700

    rabbitmq

---
+++ authen-service/src/main/java/codes/rytis/logindemo/config/rabbitmq/RabbitQueueDefine.java
@@ -0,0 +1,28 @@
+package codes.rytis.logindemo.config.rabbitmq;
+
+import codes.rytis.logindemo.constants.JobQueue;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitQueueDefine {
+    @Autowired
+    @Qualifier("amqpAdmin")
+    AmqpAdmin rabbitAdminMain;
+
+    @Bean
+    public Queue incomingQueue() {
+
+        //Define queue job
+        for (String queueName : JobQueue.queueNameList) {
+            Queue queue = new Queue(queueName, true, false, false, null);
+//            rabbitAdminMain.declareQueue(queue);
+        }
+        return null;
+    }
+
+}
