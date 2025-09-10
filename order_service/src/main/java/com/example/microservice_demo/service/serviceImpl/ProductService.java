01af0fd4409ecc6b3a6915276e7bb16f1924b724
minh11123 <93438103+Minh11123@users.noreply.github.com>
2025-09-11 02:23:27 +0700

    Add Redis cache config

--- order_service/src/main/java/com/example/microservice_demo/service/serviceImpl/ProductService.java
+++ order_service/src/main/java/com/example/microservice_demo/service/serviceImpl/ProductService.java
@@ -5,6 +5,7 @@ import com.example.microservice_demo.repository.ProductRepo;
 import com.example.microservice_demo.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class ProductService implements IProductService {
     @Autowired
     private ProductRepo productRepo;
     @Override
+    @Cacheable("products")
     public List<Product> getAll() {
         return productRepo.findAll();
     }
